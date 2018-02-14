1f93ab35d152d581fc41f9071a271e72c57d848d
DKosmos <davidakosmos@gmail.com>
2018-02-14 14:21:46 -0800

    90% done with tests

---
+++ lab-david/__test__/reader.test.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const reader = require('../lib/reader.js');
+require('jest');
+
+describe('Reader Module', function(){
+    describe('with an invalid file path', function(){
+        it('should return an error', function(){
+            reader(`${__dirname}/doesntexists.pdf`, function(err){    
+                expect(err).toBeTruthy();
+                expect(typeof err).toBe('object');
+                expect(err.code).toBe('ENOENT');
+            })
+        });
+    });
+    describe('with a valid file path', function(){
+        it('should return the first 8 bytes of the each file', function(){
+            reader([`${__dirname}/../data/one.txt`, `${__dirname}/../data/two.txt`, `${__dirname}/../data/three.txt`], function(err, data){
+               expect(err).toBe(null);
+               expect(Array.isArray(data)).toBeTruthy();
+               expect(data).toEqual([''])    
+            });
+        })
+    })
+})
