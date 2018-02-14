17b6209a3fb8b3754a35b08fd5d13275039f434b
Your Name <nords3x4@gmail.com>
2018-02-14 14:28:50 -0800

    all buffed txt files in to an array

--- lab-greg/__test__/reader.test.js
+++ lab-greg/__test__/reader.test.js
@@ -2,3 +2,4 @@
 
 const fileReader = require('../lib/reader.js');
 require('jest');
+
--- lab-greg/lib/reader.js
+++ lab-greg/lib/reader.js
@@ -6,7 +6,12 @@ let txt1 = `${__dirname}/data/one.txt`;
 let txt2 = `${__dirname}/data/two.txt`;
 let txt3 = `${__dirname}/data/three.txt`;
 
-console.log(txt1, txt2, txt3);
+
+let buff1 = new Buffer(txt1);
+let buff2 = new Buffer(txt2);
+let buff3 = new Buffer(txt3);
+
+let allData = [buff1, buff2, buff3];
 
 const fileReader = module.exports = function(file, callback) {
   fs.readFile(file, function(err, data) {
@@ -14,3 +19,4 @@ const fileReader = module.exports = function(file, callback) {
     return callback(data).toString();
   });
 };
+
