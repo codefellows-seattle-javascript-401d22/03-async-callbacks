e775840b43d659e4586f22cb1691e3f7a4ea03b9
Melanie Cohen <melaniebcohen@gmail.com>
2018-02-14 15:56:11 -0800

    more coverage and adding done

--- melanie-lab/__test__/reader-test.test.js
+++ melanie-lab/__test__/reader-test.test.js
@@ -7,44 +7,56 @@ let paths = [`${__dirname}/../data/one.txt`, `${__dirname}/../data/two.txt`, `${
 
 describe('Read Files Module', function() {
   describe('with incorrect file path', function() {
-    it('should return an error', function() {
+    it('should return an error', function(done) {
       reader(`${__dirname}/../data/onez.txt`, function(err) {
         expect(err).toBeTruthy();
         expect(typeof err).toBe('object');
+        done();
       });
     });
   });
   describe('with no data passed through', function() {
-    it('should return an error with no data', function() {
+    it('should return an error with no data', function(done) {
       reader(paths[0], function(err) {
         expect(err).toBe(null);
+        done();
       });
     });
   });
   describe('with correct file path', function() {
-    it('should return first 8 bytes of text from one.txt', function() {
+    it('should accept two arguments', function(done) {
+      expect(arguments.length === 2);
+      done();
+    });
+    it('should return first 8 bytes of text from one.txt', function(done) {
       reader(paths[0], function(err, data) {
         expect(err).toBe(null);
         expect(typeof data.buffer).toEqual('object');
+        expect(data.toString()).toEqual('One file');
         console.log('one.txt:', data);
+        done();
       });
     });
   });
   describe('with correct file path round 2', function() {
-    it('should return first 8 bytes of text from two.txt', function() {
+    it('should return first 8 bytes of text from two.txt', function(done) {
       reader(paths[1], function(err, data) {
         expect(err).toBe(null);
         expect(typeof data.buffer).toEqual('object');
+        expect(data.toString()).toEqual('File num');
         console.log('two.txt:', data);
+        done();
       });
     });
   });
   describe('with correct file path round 3', function() {
-    it('should return first 8 bytes of text from two.txt', function() {
+    it('should return first 8 bytes of text from two.txt', function(done) {
       reader(paths[2], function(err, data) {
         expect(err).toBe(null);
         expect(typeof data.buffer).toEqual('object');
+        expect(data.toString()).toEqual('The thir');
         console.log('three.txt:', data);
+        done();
       });
     });
   });
