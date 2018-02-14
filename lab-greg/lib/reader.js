088543930f1f2328107df9202975ae809b34594c
Your Name <nords3x4@gmail.com>
2018-02-14 14:16:51 -0800

    reader.js skeleton built, text files built, reader.test.js started

--- lab-greg/lib/reader.js
+++ lab-greg/lib/reader.js
@@ -1,2 +1,16 @@
 'use strict';
 
+const fs = require('fs');
+
+let txt1 = `${__dirname}/data/one.txt`;
+let txt2 = `${__dirname}/data/two.txt`;
+let txt3 = `${__dirname}/data/three.txt`;
+
+console.log(txt1, txt2, txt3);
+
+const fileReader = module.exports = function(file, callback) {
+  fs.readFile(file, function(err, data) {
+    if(err) return callback(err);
+    return callback(data).toString();
+  });
+};
