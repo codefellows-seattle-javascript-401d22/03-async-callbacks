d4a75b06924a7c8198f7b6a4df6de2c4af76305b
Brian Bixby <brianbixby0@gmail.com>
2018-02-14 18:10:58 -0800

    read me done, going to try to get higher test rate at 62% now

--- lab-brian/__test__/reader.test.js
+++ lab-brian/__test__/reader.test.js
@@ -2,8 +2,9 @@
 
 const reader = require('../lib/reader.js');
 require('jest');
+
 describe('Reader Module', function() {
-  describe('with an improper file path', function() {
+  describe('with improper arguments', function() {
     it('should throw argument data type error', function() {
       expect(function() {
         reader([`${__dirname}/../data/one.txt`, `${__dirname}/../data/two.txt`, `${__dirname}/../data/three.txt`], []);
@@ -12,6 +13,7 @@ describe('Reader Module', function() {
         reader({}, function() {});
       }).toThrow('argument data type error');
     });
+
     it('should throw argument number error', function() {
       expect(function() {
         reader([`${__dirname}/../data/one.txt`, `${__dirname}/../data/two.txt`, `${__dirname}/../data/three.txt`], function() {}, function() {});
@@ -20,6 +22,7 @@ describe('Reader Module', function() {
         reader([`${__dirname}/../data/one.txt`, `${__dirname}/../data/two.txt`, `${__dirname}/../data/three.txt`], function() {}, function() {});
       }).toThrow('must have exactly 2 arguments');
     });
+
     it('should throw array length error', function() {
       expect(function() {
         reader([`${__dirname}/../data/one.txt`, `${__dirname}/../data/two.txt`], function(){});
@@ -29,7 +32,30 @@ describe('Reader Module', function() {
       }).toThrow('file length not 3');
     });
   });
+
+  describe('with an improper file path', function() {
+    it('should return an error', function() {
+      reader([`${__dirname}/../data/dgsweg.txt`, `${__dirname}/../data/two.txt`, `${__dirname}/../data/three.txt`], function(err) {
+        expect(err).toBeTruthy();
+        expect(typeof err).toBe('object');
+        expect(err.code).toBe('ENOENT');
+      });
+    });
+  });
+
+  describe('with proper arguments and proper file path', function() {
+    it('should return file contents', function() {
+      reader([`${__dirname}/../data/one.txt`, `${__dirname}/../data/two.txt`, `${__dirname}/../data/three.txt`], function(err, data) {
+        expect(err).toBe(null);
+        expect(typeof data).toBe('object');
+        expect(data).toEqual([ '53697474696e6720', '7368652073616964', '596f752072656163' ]);
+      });
+    });
+  });
+
 });
+
+
 // it('should return an error', function(done) {
 //   reader([`${__dirname}/../data/one.txt]`], function(err) {
 //     expect(err).toBeTruthy();
