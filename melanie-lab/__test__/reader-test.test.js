c176c27261f1f641bfbe6fb4cc342af89cd790fc
Melanie Cohen <melaniebcohen@gmail.com>
2018-02-14 16:26:19 -0800

    lab complete

--- melanie-lab/__test__/reader-test.test.js
+++ melanie-lab/__test__/reader-test.test.js
@@ -15,15 +15,13 @@ describe('Read Files Module', function() {
       });
     });
   });
-  describe('with no data passed through', function() {
-    it('should return an error with no data', function(done) {
+  describe('with correct file path', function() {
+    it('should return error when no data is passed through', function(done) {
       reader(paths[0], function(err) {
         expect(err).toBe(null);
         done();
       });
     });
-  });
-  describe('with correct file path', function() {
     it('should accept two arguments', function(done) {
       expect(arguments.length === 2);
       done();
@@ -31,6 +29,7 @@ describe('Read Files Module', function() {
     it('should return first 8 bytes of text from one.txt', function(done) {
       reader(paths[0], function(err, data) {
         expect(err).toBe(null);
+        expect(typeof paths[0]).toEqual('string');
         expect(typeof data.buffer).toEqual('object');
         expect(data.toString()).toEqual('One file');
         console.log('one.txt:', data);
@@ -39,9 +38,16 @@ describe('Read Files Module', function() {
     });
   });
   describe('with correct file path round 2', function() {
+    it('should return error when no data is passed through', function(done) {
+      reader(paths[1], function(err) {
+        expect(err).toBe(null);
+        done();
+      });
+    });
     it('should return first 8 bytes of text from two.txt', function(done) {
       reader(paths[1], function(err, data) {
         expect(err).toBe(null);
+        expect(typeof paths[1]).toEqual('string');
         expect(typeof data.buffer).toEqual('object');
         expect(data.toString()).toEqual('File num');
         console.log('two.txt:', data);
@@ -50,9 +56,16 @@ describe('Read Files Module', function() {
     });
   });
   describe('with correct file path round 3', function() {
+    it('should return error when no data is passed through', function(done) {
+      reader(paths[2], function(err) {
+        expect(err).toBe(null);
+        done();
+      });
+    });
     it('should return first 8 bytes of text from two.txt', function(done) {
       reader(paths[2], function(err, data) {
         expect(err).toBe(null);
+        expect(typeof paths[2]).toEqual('string');
         expect(typeof data.buffer).toEqual('object');
         expect(data.toString()).toEqual('The thir');
         console.log('three.txt:', data);
