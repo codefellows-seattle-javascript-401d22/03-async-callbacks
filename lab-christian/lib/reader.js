070034254398f30bca38f24f8e587a8bedeea984
Christian Miller <christian.ax.miller@gmail.com>
2018-02-14 20:57:15 -0800

    incomplete

--- lab-christian/lib/reader.js
+++ lab-christian/lib/reader.js
@@ -1,2 +1,11 @@
 'use strict';
 
+const fs = require('fs');
+
+const reader = module.exports = function(paths, callback) {
+    fs.readFile(paths[0], function(err, data) {
+        if (err) return callback(err)
+
+        
+    })
+}
