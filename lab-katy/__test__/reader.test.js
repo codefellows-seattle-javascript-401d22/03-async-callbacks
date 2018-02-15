feb7abdddf0783185322bb8b0bd2a6c8f53e08db
Katy Robinson <katyorob@gmail.com>
2018-02-14 16:41:32 -0800

    lab done minus readme

--- lab-katy/__test__/reader.test.js
+++ lab-katy/__test__/reader.test.js
@@ -5,19 +5,35 @@ require('jest');
 
 describe('File Reader Module', function(){
     describe('with improper file path', function(){
-        it('should return an error', function(){
+        it('should return an error', function(done){
             fileReader(`${__dirname}/no-file.txt`, function(err){
                 expect(err).toBeTruthy();
                 expect(typeof err).toBe('object');
+                done();
             });
         });
     describe('with proper file path', function(){
-        it('should return first 8 bytes of binary info', function(){
+        it('should return first 8 bytes of binary info', function(done){
             fileReader([`${__dirname}/../data/one.txt`], function(err, data){
                 expect(err).toBe(null);
                 expect(typeof data).toBe('string');
-            })
-        })
+                done();
+            });
+        });
+        it('should return first 8 bytes of binary info for 2nd path', function(done){
+            fileReader([`${__dirname}/../data/two.txt`], function(err, data){
+                expect(err).toBe(null);
+                expect(typeof data).toBe('string');
+                done();
+            });
+        });
+        it('should return first 8 bytes of binary info for 3nd path', function(done){
+            fileReader([`${__dirname}/../data/three.txt`], function(err, data){
+                expect(err).toBe(null);
+                expect(typeof data).toBe('string');
+                done();
+            });
+        });
     })
   })
 })
--- lab-katy/lib/reader.js
+++ lab-katy/lib/reader.js
@@ -6,10 +6,25 @@ const fileReader = module.exports = function(paths, callback) {
     fs.readFile(paths[0], function(err, data){
         if (err) return callback(err);
 
-        var result = data.toString('hex', 0, 7);
-    
-        return callback(null, result);
+        var result = data.toString('hex', 0, 8);
         console.log(result);
+        return callback(null, result);
+
+        fs.readFile(paths[1], function(err, data){
+            if (err) return callback(err);
+
+            var result2 = data.toString('hex', 0, 8);
+            console.log(result2);
+            return callback(null, result2);
+
+            fs.readFile(paths[2], function(err, data){
+                if(err) return callback(err);
+
+                var result3 = data.toString('hex', 0, 8);
+                console.log(result3);
+                return callback(null, result3);
+            })
+        });
     });
 };
 
