90ca1ce149c8394a14b6b972f779fc21462b5acc
nicoleweese <nweese@ymail.com>
2018-02-16 13:20:16 -0800

    reworked solution for false positives

--- lab-nicole/index.js
+++
@@ -1,8 +0,0 @@
-// 'use strict';
-
-// const fs = require('fs');
-// const reader = require('./lib/reader.js');
-
-// var files = ['./data/one.txt', './data/two.txt', './data/three.txt'];
-
-// reader(files);
