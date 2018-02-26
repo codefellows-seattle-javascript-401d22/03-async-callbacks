2a5deea7781595f59fe093bf3572a0ddbeb1a5d5
Ahmed Ali <Ahmed98148@gmail.com>
2018-02-26 08:14:34 -0800

    almost done

--- lab-ahmed/lib/reader.js
+++ lab-ahmed/lib/reader.js
@@ -1,2 +1,38 @@
 'use strict';
 
+const fs = require('fs');
+
+const reader = module.exports = function(pathArray, callback){
+  var arr = [];
+
+  console.log('sdfsdfsdfsdf', pathArray)
+
+  fs.readFile(pathArray[0], function(err, data) {
+    console.log('pathArray:', pathArray);
+    if (err) throw err;
+    data = new Buffer(data);
+    data = data.toString('hex');
+    data = data.slice(0, 8);
+    arr.push(data);
+console.log('@', pathArray[1])
+    fs.readFile(pathArray[1], function(err, data) {
+      console.log('2');
+      if (err) throw err;
+      data = new Buffer(data);
+      data = data.toString('hex');
+      data = data.slice(0, 8);
+      arr.push(data);
+
+      fs.readFile(pathArray[2], function(err, data) {
+        console.log('3');
+        if (err) throw err;
+        data = new Buffer(data);
+        data = data.toString('hex');
+        data = data.slice(0, 8);
+        arr.push(data);
+        callback(null, arr);
+      });
+    });
+  });
+
+}
